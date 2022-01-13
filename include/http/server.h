40c82ab65f493b6f55a8c07315ee17b34d5d3d9c
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-01-14 00:11:32 +0400

    Create http server initial interface

---
+++ include/http/server.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace http
+{
+	template <typename backend_t>
+	class server
+	{
+	public:
+		using impl_t = backend_t::server_impl_t;
+
+	public:
+		void start(short port) { __backend.start(port); }
+
+		bool started() { return __backend.started(); }
+
+	private:
+		impl_t __backend;
+	};
+} // namespace http
